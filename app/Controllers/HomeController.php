bb8462cdd83ae1cebffb2542d8c1e0e792d99ccf
Ahi Les <ahilespelid@gmail.com>
2023-04-28 23:04:18 +0300

    first view full

--- app/Controllers/HomeController.php
+++ app/Controllers/HomeController.php
@@ -1,5 +1,6 @@
 <?php 
 namespace App\Controllers;
+use Btx\Crest;
 /// */ Юзаем PhpOffice 
 use PhpOffice\PhpSpreadsheet\Spreadsheet; use PhpOffice\PhpSpreadsheet\Style\Fill; use PhpOffice\PhpSpreadsheet\Style\Color;
 use PhpOffice\PhpSpreadsheet\Writer\Xlsx;
@@ -28,6 +29,7 @@ public function write():bool{
     return ((new Xlsx($spreadsheet))->save($this->file)) ? true : false;
     }
     public function render(array $data = [], string $view = 'first'):?object{
+        pa($result = \Btx\CRest::call('profile'));
         $view = str_replace(['?', '\\', '/', ':', '"', '*', '>', '<', '|'], '',$view);
         $view = dirname($_SERVER['DOCUMENT_ROOT']).DIRECTORY_SEPARATOR.'resource'.DIRECTORY_SEPARATOR.'veiw'.DIRECTORY_SEPARATOR.$view.DIRECTORY_SEPARATOR.'index.php';
 
